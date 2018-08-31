8c5df828d66408152c0c07de570d0438e72a2692
つじ <w.mjm.6561@gmail.com>
2018-08-31 16:26:07 +0900

    LssonLogic途中

--- src/bo/LessonLogic.java
+++ src/bo/LessonLogic.java
@@ -3,7 +3,11 @@ import java.util.Calendar;
 
 import model.Lesson;
 public class LessonLogic {
+
 	public Lesson showSchedule(Calendar calendar){
+		Calendar today=Calendar.getInstance();
+		int M=today.get(Calendar.MONTH);
+		int D=today.get(Calendar.DATE);
 		Lesson le= new Lesson();
 return le;
 	}
