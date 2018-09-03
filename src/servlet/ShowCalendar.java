a9ca02e1df2d9405a6f10720ba236f3d95c3dad9
つじ <w.mjm.6561@gmail.com>
2018-09-03 16:39:22 +0900

    ShowCalendar開始

--- src/servlet/ShowCalendar.java
+++ src/servlet/ShowCalendar.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.Calendar;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 public class ShowCalendar extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Calendar today=Calendar.getInstance();
+		int Y=today.get(Calendar.YEAR);
+		int M=today.get(Calendar.MONTH);
+		int D=today.get(Calendar.DATE);
+		today.set(Y,M,1);
 	}
 
 }
