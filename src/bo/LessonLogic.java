c5ef6d739679f419b3d56e39a4d3f209ac44ab9d
つじ <w.mjm.6561@gmail.com>
2018-09-03 12:50:50 +0900

    LessonDAO

--- src/bo/LessonLogic.java
+++ src/bo/LessonLogic.java
@@ -1,11 +1,17 @@
 package bo;
 import java.util.Calendar;
 
-import model.Lesson;
+import dao.LessonDAO;
+import model.Schedule;
 public class LessonLogic {
 
-	public Lesson showSchedule(Calendar calendar){
+	public Schedule showSchedule(Calendar calendar){
 		calendar=Calendar.getInstance();
+		int LY = calendar.get(Calendar.YEAR);
+		int LM = calendar.get(Calendar.MONTH);
+		int LD = calendar.get(Calendar.DATE);
+		String sLM=String.valueOf(LM);
+		String sLD=String.valueOf(LD);
 		Calendar today=Calendar.getInstance();
 		int Y=today.get(Calendar.YEAR);
 		int M=today.get(Calendar.MONTH);
@@ -13,8 +19,22 @@ public class LessonLogic {
 		today.set(Y,M,D);
 		today.add(D,2);
 		boolean N=calendar.after(today);
-		Lesson le= new Lesson();
-return le;
+		String[] le= new String[4];
+		if(N=false){
+			le[1] = sLM;
+			le[2]=sLD;
+			le[3]="締切";
+			le[4]="";
+
+
+		}else{
+			LessonDAO LDAO=new LessonDAO();
+			LDAO.findDay(sLM,sLD);
+
+
+		}
+		Schedule sle=new Schedule(le);
+return sle;
 	}
 
 }
