ae13eff858fe21b2707a5c567caa4433b27257cb
yamamoto <k.yamamoto517rs@gmail.com>
2018-09-06 15:08:35 +0900

    SQL接続共通（要変更作業有）

--- src/model/SQL.java
+++ src/model/SQL.java
@@ -2,7 +2,10 @@ package model;
 
 public class SQL {
 	final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-	final String JDBC_URL_y = "jdbc:mysql://192.168.10.27/pilates";
+	//localhostの部分をSQLファイル記載のIPアドレスに変更すると
+	//山本のSQLに接続できます。
+	//SQLファイルの場所･･･授業用\アプリケーション\おにぎり\
+	final String JDBC_URL_y = "jdbc:mysql://localhost/pilates";
 	final String DB_USER = "root";
 	final String DB_PASS = "root";
 
