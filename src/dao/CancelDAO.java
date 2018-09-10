a5325630b2f3c05ddd4bf1af3169496a373b2107
taiheikanemoto <taihei.kanemoto@gmail.com>
2018-09-10 10:34:40 +0900

    cancelDAO

--- src/dao/CancelDAO.java
+++ src/dao/CancelDAO.java
@@ -3,36 +3,45 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 import bo.ReservedLogic;
 
 public class CancelDAO {
 	public List<ReservedLogic> findAll(){
-		
+
 		Connection conn = null;
 		List<ReservedLogic> empList = new ArrayList<ReservedLogic>();
-	
+
 	try {
 		//JDBCドライバ読込み
 		Class.forName("org.h2.Driver");
-		
+
 		//データベースへ接続
 		conn = DriverManager.getConnection(
 				"JDBC_URL,DB_USER,DB_PASS");
-		
+
 		//UPDATE文の準備
-		String sql = "UPDATE INTO Reservation(number,date,mail) VALUES(?,?,?)";
+		String sql = "UPDATE INTO Reservation(yes_or_no) VALUES(?)";
 		PreparedStatement pStmt =conn.prepareStatement(sql);
-		
+
 		//UPDATE文の「?」に使用する値を設定しSQLを完成
-		pStmt.setString(1, ReservedLogic.getNumber());
-		pStmt.setString(2, ReservedLogic.getDate());
-		pStmt.setString(3, ReservedLogic.getMail());
-		
-		
-		
-	}catch
-	
+		pStmt.setInt(1, 0);
+
+	} catch (SQLException e) {
+		e.printStackTrace();
+		return null;
+	} finally {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+	}
+	}
+}
