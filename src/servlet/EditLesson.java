32bfbcd90f5b7b612416cb170383f8adb0779547
taiheikanemoto <taihei.kanemoto@gmail.com>
2018-09-10 14:36:48 +0900

    いろいろ

--- src/servlet/EditLesson.java
+++ src/servlet/EditLesson.java
@@ -1,41 +1,32 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class EditLesson
- */
 @WebServlet("/EditLesson")
 public class EditLesson extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EditLesson() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		String create = request.getParameter("create");
+		String delete = request.getParameter("delete");
+		//フォワード
+				String forwardPath = "";
+				if(create!=null) {
+					forwardPath="form.jsp";
+				}else if(delete!=null) {
+					forwardPath="/WEB-INF/jsp/cancel.jsp";
+				}
+
+				RequestDispatcher dispatcher =
+						request.getRequestDispatcher(forwardPath);
+						dispatcher.forward(request, response);
+			}
 	}
 
-}
