40298e8a9521a157780b576c075471502726f9c7
yamamoto <k.yamamoto517rs@gmail.com>
2018-09-11 16:59:56 +0900

    いろいろ

--- src/bo/EditLogic.java
+++ src/bo/EditLogic.java
@@ -4,6 +4,12 @@ import dao.LessonDAO;
 import model.Lesson;
 
 public class EditLogic {
+
+	public boolean checkLogic(Lesson lesson){
+		LessonDAO lso=new LessonDAO();
+		return lso.check(lesson);
+	}
+
 	public boolean insertLogic(Lesson lesson){
 		LessonDAO lso=new LessonDAO();
 		return lso.insert(lesson);
